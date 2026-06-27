6bcd9b49876c0719ed86828e093fe435d4c24d85
MAbdullahzafar <135574743+MAbdullahzafar@users.noreply.github.com>
2026-06-27 11:52:29 +0500

    Fix form target url redirection parameters

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,10 @@
                 <div class="card-header" style="font-weight: 700; background-color: #f8f9fa;">{{ __('Login') }}</div>
 
                 <div class="card-body" style="padding: 25px;">
-                    <!-- <form method="POST" action="{{ route('login') }}"> -->
-                        <form method="POST" action="https://vercel.app">
+                    <!-- <form method="POST" action="{{ route('login') }}">
+                        <form method="POST" action="https://vercel.app"> -->
+                            <form method="POST" action="{{ url('/login') }}">
+
 
                         @csrf
 
