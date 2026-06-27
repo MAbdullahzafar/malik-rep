502df070326dbfced247645572a47e6d3e441904
MAbdullahzafar <135574743+MAbdullahzafar@users.noreply.github.com>
2026-06-27 11:17:26 +0500

    Configure explicit serverless proxy headers

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,28 +1,17 @@
-<?php
-
-namespace App\Http\Middleware;
-
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
-
-class TrustProxies extends Middleware
-{
     /**
      * The trusted proxies for this application.
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
-}
+    protected $headers = \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+                         \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+                         \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+                         \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+                         \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB;
