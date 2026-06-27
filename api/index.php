b7dc8fbeeb66803e516e862e579f9d6b8c2185a5
MAbdullahzafar <135574743+MAbdullahzafar@users.noreply.github.com>
2026-06-27 10:07:10 +0500

    Migrate application bootstrapper entry directly into api node folder

--- api/index.php
+++ api/index.php
@@ -1,4 +1,3 @@
-<?php
-// Forward Vercel serverless requests to Laravel's main entry point
-require __DIR__ . '/../public/index.php';
+require __DIR__.'/../../vendor/autoload.php';
 
+$app = require_once __DIR__.'/../../bootstrap/app.php';
