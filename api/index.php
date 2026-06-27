baeb8ecac611149b6e64be0cf6daca0655a8a325
MAbdullahzafar <135574743+MAbdullahzafar@users.noreply.github.com>
2026-06-27 10:24:36 +0500

    Intercept package manifest paths inside entry script

--- api/index.php
+++ api/index.php
@@ -4,16 +4,26 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Check if the application is in maintenance mode
+// Force writable paths for serverless manifests
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer Autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the Request
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Bind dynamic serverless temporary instances
+$app->useStoragePath('/tmp/storage');
+$app->instance('path.config_cache', '/tmp/config.php');
+$app->instance('path.routes_cache', '/tmp/routes.php');
+$app->instance('path.services_cache', '/tmp/services.php');
+$app->instance('path.packages_cache', '/tmp/packages.php');
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
