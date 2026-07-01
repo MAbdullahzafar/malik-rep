e57416edb536f33da9e6234fdbecd9729feed9dc
MAbdullahzafar <135574743+MAbdullahzafar@users.noreply.github.com>
2026-07-01 10:32:42 +0500

    Fix PSR-4 class naming conventions

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class welcomeEmail extends Mailable
+class WelcomeEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
